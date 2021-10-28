dfb71bf8660915fb32f98c087e9d661b5f181db9
Farouk Allani <farouk.allani.pro@gmail.com>
2021-10-28 16:36:36 +0200

    last touch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
 import './style.css';
 import imageInSrc from './imageInSrc.jpg';
 
@@ -9,16 +7,16 @@ function App() {
       <header className="App-header">
       <div>
         <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
-          <h1 className="title red">Your name here</h1>
+          <h1 className="title red">Farouk Allani</h1>
           <br />
-          <img src={imageInSrc} alt="inSrc" width={320} height={240} />
+          <img src={imageInSrc} alt="inSrc" width={420} height={240} />
           <br />
           <> 
-          <img src='/images/imageInPublic.jpg'  width={320} height={240} /> 
+          <img src='/images/imageInPublic.jpg' alt='public'  width={420} height={240} /> 
           </>
         </div>
         <video width={320} height={240} controls>
-          <source src="myVideo.mp4" type="video/mp4" />
+          <source src="/myVideo.mp4" type="video/mp4" autoPlay loop />
         </video>
       </div>
       </header>
